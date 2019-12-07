2b4f98cdbc3b3efb0fb5eeb2270c2b0431759e9d
username <user@email.mail>
2019-12-08 00:11:43 +0900

    architecture

--- NFVPortal/views.py
+++ NFVPortal/views.py
@@ -43,20 +43,10 @@ Created on 2016. 7. 8.
 from django.template import loader
 from django.http import HttpResponse
 
-from ciscowebkit.common.engine import Engine
-
-
 # Create your views here.
-
-def action(request):
-    return Engine.GET().__action__(request)
 
 def test(request):
     return HttpResponse(loader.get_template('test.html').render())
-
-class DisableCSRF(object):
-    def process_request(self, request):
-        setattr(request, '_dont_enforce_csrf_checks', True)
 
 
 
